dadd86a67d53e1cac21778fee185671e962419ed
rubytree1729 <rubytree1729@gmail.com>
2022-02-21 17:01:44 +0900

    change port to 3000

--- index.ts
+++ index.ts
@@ -5,14 +5,9 @@ import mongoose from "mongoose"
 import routes from './src/routes'
 
 const app = express()
-const corsOptions = { // CORS 옵션
-    origin: 'http://localhost:3000',
-    credentials: true
-}
-const CONNECT_URL = 'mongodb://localhost:27017/test'
+const CONNECT_URL = 'mongodb://127.0.0.1:27017/test'
 mongoose.connect(CONNECT_URL).then(() => console.log("mongodb connected ..."))
     .catch(e => console.log(`failed to connect mongodb: ${e}`))
-app.use(cors(corsOptions)) // CORS 설정
 app.use(express.json()) // request body 파싱
 app.use(logger("dev")) // Logger 설정
 app.use("/api", routes) // api 라우팅
@@ -26,6 +21,6 @@ app.use(((err, req, res, next) => { // 서버 내부 오류 처리
     console.error(err.stack)
     res.status(500).send("something is broken on server !")
 }) as ErrorRequestHandler)
-app.listen(5000, () => { // 5000 포트로 서버 오픈
+app.listen(3000, () => { // 3000 포트로 서버 오픈
     console.log('server is running on port 3000 ...')
 })
